8ac94d7211efcc0a6a6d5693463fc1dfc233d584
Kendi Anyika <kendi.anyika@gmail.com>
2023-02-28 13:24:27 +0300

    Implementing the homepage front end

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Import Statements and general set up
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const { readFileSync, writeFileSync } = require('fs')
@@ -5,19 +6,26 @@ const bodyParser = require('body-parser')
 const urlEncoder = bodyParser.urlencoded({extended:true})
 const jsonParser = bodyParser.json()
 const path = require('path')
-
 const app = express();
 const PORT = 3000;
 
+// Basic set up
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
+
+app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(urlEncoder);
-
 app.use(cookieParser());
 
+// Landing page route
+app.get('/', (req, res) => {
+    res.render('landing', { root: __dirname });
+  });
+
+// Running server Message
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
 
 
