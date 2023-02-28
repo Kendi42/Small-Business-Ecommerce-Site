27f13a0f0887a7025d8fe691965ed0fc62f06a89
Kendi Anyika <kendi.anyika@gmail.com>
2023-02-28 14:48:32 +0300

    Linking landing page, login and signup

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const path = require('path')
 const app = express();
 const PORT = 3000;
 
-// Basic set up
+// Setting up the routing for the views
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-
+// Routing for static files
 app.use(express.static(__dirname + '/public'));
 
 app.use(express.json());
@@ -22,8 +22,19 @@ app.use(cookieParser());
 
 // Landing page route
 app.get('/', (req, res) => {
-    res.render('login', { root: __dirname });
+    res.render('landing', { root: __dirname });
   });
+
+// Go to login page
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+// Go to signup page
+app.get("/signup", (req, res) => {
+	res.render("signup");
+});
+
 
 // Running server Message
 app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
